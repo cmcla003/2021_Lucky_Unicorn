47bb1a8156d6fc7afc189c47e6763692a394e755
cmcla003 <51941943+cmcla003@users.noreply.github.com>
2021-05-17 14:17:00 +1200

    Looping and Name input

---
+++ 00_looping.py
@@ -0,0 +1,10 @@
+# count up from 1 to 10
+
+for item in range(1 , 11):
+    print(item)
+print()
+
+greeting=("Hello World")
+
+for letter in greeting:
+    print(letter)
---
+++ 00_sandbox.py
@@ -0,0 +1,10 @@
+
+# get name until answer is entered
+name = ""
+
+while name != "xxx":
+     name = input("Who are you?")
+     print(name)
+
+print()
+print("We are done")
