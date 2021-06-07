7cdc2bc5097f4e2116c3f37937d894a8a8ccae47
cmcla003 <mclarenc@awatapu.school.nz>
2021-06-08 10:40:19 +1200

    Comment code in integrate token comp in main

--- 00_LU_Base_v1.py
+++ 00_LU_Base_v1.py
@@ -1,3 +1,6 @@
+# Lucky Unicorn Base code
+import random
+
 # Function goes here
 def yes_no(question):
     valid = False
@@ -54,5 +57,31 @@ print("Program continues")
 
 #Ask user how much they want to play with
 how_much = num_check("How much do you want to play with? ", 0, 10)
-
 print("You will be spending ${}".format(how_much))
+
+STARTING_BALANCE = how_much
+balance = STARTING_BALANCE
+
+# generates random number that is wthen assigned to tokens
+chosen_num = random.randint(1,100)
+
+# adjust balance based on random number drawn
+# between 1 & 5 user gets unicorn and $4
+if 1 <= chosen_num <= 5:
+    chosen ="unicorn"
+    balance +=4
+# if between 6 and 36 user gets donkey and loses $1
+elif 6>= chosen_num <= 36:
+    chosen = "donkey"
+    balance -= 1
+else:
+    # if even number outside the above ranges a horse is drawn .50c increase
+    if chosen_num % 2 == 0:
+        chosen = "horse "
+    # every other situation will draw a zebra
+    else:
+        chosen = "zebra"
+    balance -= 0.5
+
+print("you got a {}. Your balance is {:.2f}".format(chosen,balance))
+
--- 03_token_generator_v4.py
+++ 03_token_generator_v4.py
@@ -8,18 +8,23 @@ balance = STARTING_BALANCE
 
 # testing loop to generate 100 tokens
 for item in range (0, 100):
+    # generates random number that is wthen assigned to tokens
     chosen_num = random.randint(1,100)
 
-    # adjust balance
+    # adjust balance based on random number drawn
+    # between 1 & 5 user gets unicorn and $4
     if 1 <= chosen_num <= 5:
         chosen ="unicorn"
         balance +=4
+    # if between 6 and 36 user gets donkey and loses $1
     elif 6>= chosen_num <= 36:
         chosen = "donkey"
         balance -= 1
     else:
+        # if even number outside the above ranges a horse is drawn .50c increase
         if chosen_num % 2 == 0:
             chosen = "horse "
+        # every other situation will draw a zebra
         else:
             chosen = "zebra"
         balance -= 0.5
