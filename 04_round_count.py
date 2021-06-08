f74d22c1115393a581677c15343916326c28e707
cmcla003 <mclarenc@awatapu.school.nz>
2021-06-08 15:56:04 +1200

    round counter with formatting

--- 04_round_count.py
+++ 04_round_count.py
@@ -2,17 +2,18 @@
 balance = 5
 rounds_played = 0
 
-play_again = input("Press <enter> to play....")
+play_again = input("Press <enter> to play....").lower()
 while play_again == "":
     rounds_played +=1
-    print("Rounds Played: " ,rounds_played)
+    print("*** Round number {} *** ".format(rounds_played))
 
     balance -=1
     print("Balance: $", balance)
 
-    if balance == 0:
+    if balance < 1:
+        play_again = "xxx"
         print("Sorry you have run out of money")
-        break
 
-    play_again = input("Press <enter> to play again or xxx to quit")
+    else:
+        play_again = input("Press <enter> to play again or xxx to quit")
 print("Your final balance is ${:.2f}".format(balance))
