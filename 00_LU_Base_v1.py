602106a8584d7a6dac112d56f8a7ef49703610b9
cmcla003 <mclarenc@awatapu.school.nz>
2021-06-08 16:27:05 +1200

    Token decoration added

--- 00_LU_Base_v1.py
+++ 00_LU_Base_v1.py
@@ -84,26 +84,34 @@ print("You will be spending ${}".format(how_much))
 STARTING_BALANCE = how_much
 balance = STARTING_BALANCE
 
+for item in range (0, 100):
 # generates random number that is wthen assigned to tokens
-chosen_num = random.randint(1,100)
+    chosen_num = random.randint(1,100)
 
-# adjust balance based on random number drawn
-# between 1 & 5 user gets unicorn and $4
-if 1 <= chosen_num <= 5:
-    chosen ="unicorn"
-    balance +=4
-# if between 6 and 36 user gets donkey and loses $1
-elif 6>= chosen_num <= 36:
-    chosen = "donkey"
-    balance -= 1
-else:
-    # if even number outside the above ranges a horse is drawn .50c increase
-    if chosen_num % 2 == 0:
-        chosen = "horse "
-    # every other situation will draw a zebra
+    # adjust balance based on random number drawn
+    # between 1 & 5 user gets unicorn and $4
+    if 1 <= chosen_num <= 5:
+        chosen ="unicorn"
+        prize_decoration = "!"
+        balance +=4
+    # if between 6 and 36 user gets donkey and loses $1
+    elif 6>= chosen_num <= 36:
+        chosen = "donkey"
+        prize_decoration = "D"
+        balance -= 1
     else:
-        chosen = "zebra"
-    balance -= 0.5
+        # if even number outside the above ranges a horse is drawn .50c increase
+        if chosen_num % 2 == 0:
+            chosen = "horse "
+            prize_decoration = "H"
+        # every other situation will draw a zebra
+        else:
+            chosen = "zebra"
+            prize_decoration ="Z"
+        balance -= 0.5
 
-print("you got a {}. Your balance is {:.2f}".format(chosen,balance))
+    outcome = "You got a {}.  Your balance is ${:.2f}".format (chosen,balance)
+
+    statement_gen(outcome, prize_decoration)
+    print ()
 
